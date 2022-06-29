f83c44bfb81a3cf1415ffa1c1f674815421b62fc
QuangHuy111297 <dev.quanghuy1112@gmail.com>
2022-06-29 17:59:04 +0700

    update

---
+++ src/components/TodoList/TodoSlice.js
@@ -0,0 +1,22 @@
+const initState = [
+  { id: 1, name: 'Learn React', completed: false, priority: 'Medium' },
+  { id: 2, name: 'Learn Redux', completed: true, priority: 'High' },
+  { id: 3, name: 'Learn Java', completed: false, priority: 'Low' },
+]
+
+const todoListReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'todoList/addTodo':
+      return [...state, action.payload]
+    case 'todoList/toggleTodoStatus':
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
+    default:
+      return state
+  }
+}
+
+export default todoListReducer
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,80 @@
+import { Col, Row, Input, Button, Select, Tag } from 'antd'
+import Todo from '../Todo'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo } from '../../redux/actions'
+import { v4 as uuidv4 } from 'uuid'
+import { useState } from 'react'
+import { todosRemainingSelector } from '../../redux/selector'
+
+export default function TodoList() {
+  const [todoName, setTodoName] = useState('')
+  const [priority, setPriority] = useState('Medium')
+
+  const todoList = useSelector(todosRemainingSelector)
+  // const searchText = useSelector(searchTextSelector)
+
+  // console.log({ todoList, searchText })
+
+  const dispatch = useDispatch()
+
+  const handleAddButtonClick = () => {
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        name: todoName,
+        priority: priority,
+        completed: false,
+      })
+    )
+
+    setTodoName('')
+    setPriority('Medium')
+  }
+
+  const handleInputChange = (e) => {
+    setTodoName(e.target.value)
+  }
+
+  const handlePiorityChange = (value) => {
+    setPriority(value)
+  }
+
+  return (
+    <Row style={{ height: 'calc(100% - 40px)' }}>
+      <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
+        {todoList.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            name={todo.name}
+            priority={todo.priority}
+            completed={todo.completed}
+          />
+        ))}
+      </Col>
+      <Col span={24}>
+        <Input.Group style={{ display: 'flex' }} compact>
+          <Input value={todoName} onChange={handleInputChange} />
+          <Select
+            defaultValue="Medium"
+            value={priority}
+            onChange={handlePiorityChange}
+          >
+            <Select.Option value="High" label="High">
+              <Tag color="red">High</Tag>
+            </Select.Option>
+            <Select.Option value="Medium" label="Medium">
+              <Tag color="blue">Medium</Tag>
+            </Select.Option>
+            <Select.Option value="Low" label="Low">
+              <Tag color="gray">Low</Tag>
+            </Select.Option>
+          </Select>
+          <Button type="primary" onClick={handleAddButtonClick}>
+            Add
+          </Button>
+        </Input.Group>
+      </Col>
+    </Row>
+  )
+}
